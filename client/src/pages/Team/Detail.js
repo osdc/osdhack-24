7606ca2295376514363be2b812d0ddb98bf87466
mehul1409 <mehulbansalswm1234@gmail.com>
2024-04-09 19:19:33 +0530

    add team members

--- client/src/pages/Team/Detail.js
+++ client/src/pages/Team/Detail.js
@@ -1,38 +1,38 @@
 const Detail = [
     {
-        img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
+        img:'/images/pikachu.jpeg',
+        name:'PIKACHU',
+        role:'  AWESOMENESS COORDINATOR',
+        social:['https://google.com','https://google.com','https://google.com'],
+    },
+    {
+        img:'/images/akshit-sir.jpeg',
+        name:'AKSHIT TYAGI',
+        role:'TECHNICAL COORDINATOR',
+        social:['https://google.com','https://google.com','https://google.com'],
+    },
+    {
+        img:'/images/shivang-sir.jpeg',
+        name:'SHIVANG SHARMA',
+        role:'STUDENT COORDINATOR',
+        social:['https://google.com','https://google.com','https://google.com'],
+    },
+    {
+        img:'/images/madhav-sir.jpeg',
+        name:'MADHAV MEHNDIRATTA',
+        role:'PROGRAM COORDINATOR',
+        social:['https://google.com','https://google.com','https://google.com'],
+    },
+    {
+        img:'/images/yuvraj.jpeg',
+        name:'YUVRAJ RATHI',
+        role:'0xRathi',
         social:['https://google.com','https://google.com','https://google.com'],
     },
     {
         img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
-        social:['https://google.com','https://google.com','https://google.com'],
-    },
-    {
-        img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
-        social:['https://google.com','https://google.com','https://google.com'],
-    },
-    {
-        img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
-        social:['https://google.com','https://google.com','https://google.com'],
-    },
-    {
-        img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
-        social:['https://google.com','https://google.com','https://google.com'],
-    },
-    {
-        img:'/images/mehul.jpg',
-        name:'Mehul Bansal',
-        role:'Student',
+        name:'MEHUL BANSAL',
+        role:'',
         social:['https://google.com','https://google.com','https://google.com'],
     },
     {
